de9948893d5eba99ab1ddf12cba1bb5e625862f1
Hao Chen <haoel@hotmail.com>
2014-02-21 15:05:37 +0800

    refactory

--- liner.py
+++ liner.py
@@ -19,31 +19,3 @@ def liner_regress(x, y, type=1):
     return liney, slope, intercept 
 
 
-if __name__ == '__main__':
-
-    from readjson import read_json
-    cpu, cpu_time, mem, mem_time, load, load_time = read_json("host2.json")
-    
-    x = asarray(cpu_time)
-    y = cpu
-    
-    # --------------------------------------------------
-    line1, _, _ =  liner_regress(x, y, type=1)
-    # --------------------------------------------------
-    line2, _, _ = liner_regress(x, y, type=0) 
-    # --------------------------------------------------
-    
-    import matplotlib
-    matplotlib.use('Agg')
-    from pylab import plot,show,savefig
-    import matplotlib.pyplot as plt
-    fig = plt.gcf()
-    fig.set_size_inches(22.5,4.5)
-    
-    plot(x,line1,'r-',x, line2, 'g-', x,y,'b-')
-    
-    savefig("liner")
-    print "Successfully created picture file liner.png"
-
-
-
--- poly.py
+++ poly.py
@@ -8,30 +8,3 @@ def poly_regression(x, y, d, module=False):
     else:
         return poly1d(m)(x) 
 
-if __name__ == '__main__':
-
-    from readjson import read_json
-    cpu, cpu_time, mem, mem_time, load, load_time = read_json("host2.json")
-    
-    x = asarray(cpu_time)
-    y = cpu
-    n = len(y)
-    
-    d = 4 
-    print  poly_regression(x, y, d, True)
-    polyline = poly_regression(x, y, d)
-    
-    import matplotlib
-    matplotlib.use('Agg')
-    from pylab import *
-    import matplotlib.pyplot as plt
-    fig = plt.gcf()
-    fig.set_size_inches(22.5,4.5)
-    
-    plot(x, y,    label="raw data")
-    plot(x, polyline, label="polyfit")
-    
-    legend()
-    
-    savefig("poly")
-    print "Successfully created picture file poly.png"
