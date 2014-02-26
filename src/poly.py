f7d6cd38682b47c5d930859a2351ddfbd78d097c
Hao Chen <haoel@hotmail.com>
2014-02-26 15:35:54 +0800

    refactory

--- src/poly.py
+++ src/poly.py
@@ -1,10 +1,7 @@
 from numpy import *
 from numpy.random import normal
 
-def poly_regression(x, y, d, module=False):
+def poly_regression(x, y, d ):
     m = polyfit(x,y,d)
-    if module :
-        return poly1d(m)    
-    else:
-        return poly1d(m)(x) 
+    return  poly1d(m)(x),  poly1d(m)
 
